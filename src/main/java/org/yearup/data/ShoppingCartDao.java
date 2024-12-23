a7b02526234944b05cfc7d65623888600567595f
BiancaMadrid <bugamadrid111@gmail.com>
2024-12-23 11:42:59 -0700

    Edit to user

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -4,6 +4,19 @@ import org.yearup.models.ShoppingCart;
 
 public interface ShoppingCartDao
 {
+public interface ShoppingCartDao {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+}
+
+    // Add additional method signatures here (without body)
+    void saveOrUpdate(ShoppingCart shoppingCart);
+
+    void clearCart(int userId);
+
+    void removeItem(int userId, int productId);
+
+    void addProductToCart(int userId, int productId);
+
+    void updateProductQuantity(int userId, int productId, int quantity);
 }
--- src/main/java/org/yearup/models/ShoppingCart.java
+++ src/main/java/org/yearup/models/ShoppingCart.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public class ShoppingCart
 {
+    public Profile user;
     private Map<Integer, ShoppingCartItem> items = new HashMap<>();
 
     public Map<Integer, ShoppingCartItem> getItems()
