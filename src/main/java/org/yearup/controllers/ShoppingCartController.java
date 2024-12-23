ff61a3db2ad8d8e0a3a28dc395d9dc9938a3beca
BiancaMadrid <bugamadrid111@gmail.com>
2024-12-23 11:46:53 -0700

    updating my shopping controller -adding annotations

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,14 +1,23 @@
 package org.yearup.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 // convert this class to a REST controller
 // only logged in users should have access to these actions
@@ -18,7 +27,23 @@ public class ShoppingCartController
     private ShoppingCartDao shoppingCartDao;
     private UserDao userDao;
     private ProductDao productDao;
+// only logged-in users should have access to these actions
+@RestController
+@RequestMapping("/cart")
+@PreAuthorize("isAuthenticated()")
+public class ShoppingCartController {
+    private static final Logger logger = LoggerFactory.getLogger(ShoppingCartController.class);
 
+    private final ShoppingCartDao shoppingCartDao;
+    private final UserDao userDao;
+    private final ProductDao productDao;
+
+    @Autowired
+    public ShoppingCartController(ShoppingCartDao shoppingCartDao, UserDao userDao, ProductDao productDao) {
+        this.shoppingCartDao = shoppingCartDao;
+        this.userDao = userDao;
+        this.productDao = productDao;
+    }
 
 
     // each method in this controller requires a Principal object as a parameter
